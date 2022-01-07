e54a22d47b86eb83ecf9494ae34bf21fb43a7b97
ishmeister <497594+ishmeister@users.noreply.github.com>
2022-01-07 20:32:59 +0000

    Restructured the paramtric_eq directory so github doesn't truncate the list in the UI

--- get_parametric_eqs.sh
+++ get_parametric_eqs.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+set -e
 
 for file in ./results/*/*ParametricEQ.txt; do
+    filename=$(basename "$file")
     result_dir=$(dirname "$file")
     dest_name=$(echo "$result_dir" | cut -d'/' -f3-)
     dest_file="${dest_name}.txt"
+    model_name=$(echo "$filename" | sed -e "s/ ParametricEQ.txt$//")
+
+    if [ ! -d "./parametric_eqs/$model_name" ]; then
+        mkdir "./parametric_eqs/$model_name"
+    fi
+
     echo "Getting '$dest_file'"
-    cp "$file" "./parametric_eqs/$dest_file"
+    cp "$file" "./parametric_eqs/$model_name/$dest_file"
 done;
