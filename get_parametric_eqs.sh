3cfb96a8b7fe46270e99c4b64090ac39eff650e5
ishmeister <497594+ishmeister@users.noreply.github.com>
2022-01-07 20:02:31 +0000

    Fixed bug in peq export script

--- get_parametric_eqs.sh
+++ get_parametric_eqs.sh
@@ -3,7 +3,7 @@
 for file in ./results/*/*ParametricEQ.txt; do
     result_dir=$(dirname "$file")
     dest_name=$(echo "$result_dir" | cut -d'/' -f3-)
-    dest_file="${dest_name}.csv"
+    dest_file="${dest_name}.txt"
     echo "Getting '$dest_file'"
     cp "$file" "./parametric_eqs/$dest_file"
 done;
